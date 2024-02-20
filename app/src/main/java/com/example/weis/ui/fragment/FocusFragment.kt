a4b1f87834d01107eb8990570f3471771da873b9
Abhijaan <abhijaanganguly02@gmail.com>
2024-02-21 00:38:39 +0530

    tab layout view pager setup

---
+++ app/src/main/java/com/example/weis/ui/fragment/FocusFragment.kt
@@ -0,0 +1,23 @@
+package com.example.weis.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weis.R
+import com.example.weis.databinding.FragmentFocusBinding
+
+class FocusFragment : Fragment() {
+
+    private lateinit var binding : FragmentFocusBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFocusBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
